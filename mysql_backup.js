de486e7564ca2bf1e2fdcc084245c5b39d881340
m2faridi <m2faridi@yahoo.com>
2023-08-21 23:59:09 +0330

    Update mysql_backup.js

--- mysql_backup.js
+++ mysql_backup.js
@@ -34,190 +34,184 @@ if (!err) {
 
 }
 });
+const insertDb = 1;//multi insert
 const program = async () => {
 
 
-const instance = new MySQLEvents(masterDb, {
-  startAtEnd: true,
-  excludedSchemas: {
-    mysql: true,
-  },
-});
+  const instance = new MySQLEvents(masterDb, {
+    startAtEnd: true,
+    excludedSchemas: {
+      mysql: true,
+    },
+  });
 
-await instance.start();
+  await instance.start();
 
-instance.addTrigger({
-  name: 'TEST',
-  expression: '*',
-  statement: MySQLEvents.STATEMENTS.ALL,
-  onEvent: (event) => {
+  instance.addTrigger({
+    name: 'TEST',
+    expression: '*',
+    statement: MySQLEvents.STATEMENTS.ALL,
+    onEvent: (event) => { // You will receive the events here
 
-    if (event.table == "tbl_ignore") return;
+      if (event.table == "notifcation_sender") return;
 
-    let query = "";
+      let query = "";
 
-    let value = "";
+      let value = "";
 
-    for (let i = 0; i < event.affectedRows.length; i++) {
-      let valueForUpdate = "";
-      let valueForDelete = "";
+      for (let i = 0; i < event.affectedRows.length; i++) {
+        let valueForUpdate = "";
+        let valueForDelete = "";
 
-      let arraykeysAndValues = [];
-      if (event.type == "INSERT" || event.type == "UPDATE") arraykeysAndValues = event.affectedRows[i].after;
-      if (event.type == "DELETE") arraykeysAndValues = event.affectedRows[i].before;
+        let arraykeysAndValues = [];
+        if (event.type == "INSERT" || event.type == "UPDATE") arraykeysAndValues = event.affectedRows[i].after;
+        if (event.type == "DELETE") arraykeysAndValues = event.affectedRows[i].before;
 
-      let keys = Object.keys(arraykeysAndValues);
-      let values = Object.values(arraykeysAndValues);
+        let keys = Object.keys(arraykeysAndValues);
+        let values = Object.values(arraykeysAndValues);
 
-      if (event.type == "UPDATE" || event.type == "DELETE") {
-        value = "";
-        query = "";
-      }
-      value += "("
-      let key = "("
+        if (event.type == "UPDATE" || event.type == "DELETE") {
+          value = "";
+          query = "";
+        }
+        value += "("
+        let key = "("
 
-      for (let i2 = 0; i2 < keys.length; i2++) {
+        for (let i2 = 0; i2 < keys.length; i2++) {
 
-        let _value = values[i2];
+          let _value = values[i2];
+          _value = mirorreDb.escape(_value);
 
-        if (_value == 1.1125369292536007e-308) {
-          _value = 0;
+
+
+          if (_value === null) {
+            valueForUpdate += " `" + keys[i2] + "` =" + _value;
+            valueForDelete += " `" + keys[i2] + "` =" + _value;
+            value += _value
+
+          } else {
+            valueForUpdate += " `" + keys[i2] + "` =" + _value + "";
+            valueForDelete += " `" + keys[i2] + "` =" + _value + "";
+            value += "" + _value + ""
+
+          }
+          if (i2 != values.length - 1) {
+            valueForUpdate += ","
+            valueForDelete += " AND "
+          }
+          key += "`" + keys[i2] + "`"
+          if (i2 != keys.length - 1) {
+            key += ","
+          }
+
+          if (i2 != values.length - 1) {
+            value += ","
+          }
         }
 
-        _value = mirorreDb.escape(_value);
+        key += ")";
+        value += ")";
 
+        if (i != event.affectedRows.length - 1) {
+          if (event.type == "INSERT") {
+            value += ",";
 
-
-        if (_value === null) {
-          valueForUpdate += " `" + keys[i2] + "` =" + _value;
-          valueForDelete += " `" + keys[i2] + "` =" + _value;
-          value += _value
-
-        } else {
-          valueForUpdate += " `" + keys[i2] + "` =" + _value + "";
-          valueForDelete += " `" + keys[i2] + "` =" + _value + "";
-          value += "" + _value + ""
-
-        }
-        if (i2 != values.length - 1) {
-          valueForUpdate += ","
-          valueForDelete += " AND "
-        }
-        key += "`" + keys[i2] + "`"
-        if (i2 != keys.length - 1) {
-          key += ","
+          }
         }
 
-        if (i2 != values.length - 1) {
-          value += ","
+        if (event.type == "UPDATE") {
+          if (event.affectedRows[i].before.id != event.affectedRows[i].after.id) {
+            query = "UPDATE `" + event.table + "` SET " + valueForUpdate + " ";
+            query += "WHERE `id` = '" + event.affectedRows[i].before.id + "';";
+          } else {
+            query = "INSERT INTO `" + event.table + "` " + key + " VALUES ";
+            query += value;
+            query += " ON DUPLICATE KEY UPDATE " + valueForUpdate + " ;";
+          }
+          arrayQuery.push(query);
         }
-      }
 
-      key += ")";
-      value += ")";
-
-      if (i != event.affectedRows.length - 1) {
         if (event.type == "INSERT") {
-          value += ",";
-
-        }
-      }
-
-      if (event.type == "UPDATE") {
-        if (event.affectedRows[i].before.id != event.affectedRows[i].after.id) {
-          query = "UPDATE `" + event.table + "` SET " + valueForUpdate + " ";
-          query += "WHERE `id` = '" + event.affectedRows[i].before.id + "';";
-        } else {
           query = "INSERT INTO `" + event.table + "` " + key + " VALUES ";
+
           query += value;
-          query += " ON DUPLICATE KEY UPDATE " + valueForUpdate + " ;";
-        }
-        arrayQuery.push(query);
-      }
 
+        }
+
+        if (event.type == "DELETE") {
+
+          if (arraykeysAndValues.id != null) {
+            query = "DELETE FROM `" + event.table + "` WHERE `id`= " + arraykeysAndValues.id;
+          } else {
+            query = "DELETE FROM `" + event.table + "` WHERE " + valueForDelete;
+
+          }
+          arrayQuery.push(query);
+        }
+      }
       if (event.type == "INSERT") {
-        query = "INSERT INTO `" + event.table + "` " + key + " VALUES ";
-
-        query += value;
-
-      }
-
-      if (event.type == "DELETE") {
-
-        if (arraykeysAndValues.id != null) {
-          query = "DELETE FROM `" + event.table + "` WHERE `id`= " + arraykeysAndValues.id;
-        } else {
-          query = "DELETE FROM `" + event.table + "` WHERE " + valueForDelete;
-
-        }
+        query += ";"
         arrayQuery.push(query);
+
       }
-    }
-    if (event.type == "INSERT") {
-      query += ";"
-      arrayQuery.push(query);
 
-    }
+    },
+  });
 
-  },
-});
-
-instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
-instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
+  instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
+  instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
 };
 
 program()
-.then(() => console.log('Waiting for database events...'))
-.catch(console.error);
-const insertDb = 1;
+  .then(() => console.log('Waiting for database events...'))
+  .catch(console.error);
 var isQuery = false;
 setInterval(function A() {
 
 
-if (arrayQuery.length != 0 && isQuery == false) {
-  if (arrayQuery.length != 0) {
-    console.log(arrayQuery.length);
-  }
-  isQuery = true;
-  var querys = "";
-  var count = insertDb;
+  if (arrayQuery.length != 0 && isQuery == false) {
+    if (arrayQuery.length != 0) {
+      console.log(arrayQuery.length);
+    }
+    isQuery = true;
+    var querys = "";
+    var count = insertDb;
 
-  if (arrayQuery.length < insertDb) {
-    count = arrayQuery.length;
-  }
-
-  for (var i = 0; i < count; i++) {
-    querys += arrayQuery[i];
-  }
-  // console.log(querys);
-  mirorreDb.getConnection(function (err, connection) {
-    if (!err) {
-
-      connection.query(querys, function (error, results, fields) {
-        // When done with the connection, release it.
-        connection.release();
-
-        if (error) {
-          console.dir(error, { depth: null }); // `depth: null` ensures unlimited recursion
-          if (error.message.indexOf("Duplicate entry") != -1) {
-            arrayQuery.splice(0, count);
-          }
-        } else {
-          arrayQuery.splice(0, count);
-
-        }
-        isQuery = false;
-        // Don't use the connection here, it has been returned to the pool.
-      });
-    } else {
-      console.dir(err, { depth: null }); // `depth: null` ensures unlimited recursion
-      isQuery = false;
-
+    if (arrayQuery.length < insertDb) {
+      count = arrayQuery.length;
     }
 
-  });
+    for (var i = 0; i < count; i++) {
+      querys += arrayQuery[i];
+    }
+    // console.log(querys);
+    mirorreDb.getConnection(function (err, connection) {
+      if (!err) {
+
+        connection.query(querys, function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+
+          if (error) {
+            console.dir(error, { depth: null }); // `depth: null` ensures unlimited recursion
+            if (error.message.indexOf("Duplicate entry") != -1) {
+              arrayQuery.splice(0, count);
+            }
+          } else {
+            arrayQuery.splice(0, count);
+
+          }
+          isQuery = false;
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      } else {
+        console.dir(err, { depth: null }); // `depth: null` ensures unlimited recursion
+        isQuery = false;
+
+      }
+
+    });
 
 
-}
+  }
 }, 50);
-
